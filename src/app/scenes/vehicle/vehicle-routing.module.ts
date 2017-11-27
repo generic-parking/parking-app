007511b0615af4430fda15b3339017d27a119cf1
douglas.silva <douglas.silva@cwi.com.br>
2017-11-27 15:17:50 -0200

    'Módulo de rotas para veiculos adicionada'

---
+++ src/app/scenes/vehicle/vehicle-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { VehicleSearchComponent } from './vehicle-search/vehicle-search.component';
+import { VehicleRegisterComponent } from './vehicle-register/vehicle-register.component';
+
+const routes = [
+  { path: '', component: VehicleSearchComponent },
+  { path: '/register', component: VehicleRegisterComponent },
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class VehicleRoutingModule { }
--- src/app/scenes/vehicle/vehicle.module.ts
+++ src/app/scenes/vehicle/vehicle.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleRegisterComponent } from './vehicle-register/vehicle-register.component';
 import { VehicleSearchComponent } from './vehicle-search/vehicle-search.component';
+import { VehicleRoutingModule } from './vehicle-routing.module';
 
 @NgModule({
+  declarations: [VehicleRegisterComponent, VehicleSearchComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    VehicleRoutingModule,
   ],
-  declarations: [VehicleRegisterComponent, VehicleSearchComponent]
 })
 export class VehicleModule { }
