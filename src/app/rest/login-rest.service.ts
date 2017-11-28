1e4cd4ae0b0d4a0e580d2e9f739d19097428bd86
douglas.silva <douglas.silva@cwi.com.br>
2017-11-28 15:52:25 -0200

    'Login está funcionando mas não redirecionando'

--- src/app/rest/login-rest.service.ts
+++ src/app/rest/login-rest.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http,  Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Login } from '../scenes/login/model/login';
 
 @Injectable()
@@ -8,8 +8,14 @@ export class LoginRestService {
   constructor(private http: Http) { }
 
   doLogin(login: Login) {
-   return this.http.post('/get/login', login).subscribe(data => {
-    return data.json()
-   });
+    const formData = "username=" + login.username + "&password=" + login.password;
+    const headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post('/api/login', formData,  options).subscribe(data => {
+      console.log(data);
+      return data.json()
+    });
   }
+
 }
